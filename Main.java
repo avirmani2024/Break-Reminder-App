33d091f79b0c971ca6698ea266851917f590de83
avirmani2024 <63978819+avirmani2024@users.noreply.github.com>
2023-04-16 23:58:29 -0400

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,31 +1,31 @@
 import java.util.Scanner;
-import java.util.Timer;
-import java.util.TimerTask;
-public class Main
-{
-public static void main(String[] args) {
-    Scanner scanner = new Scanner(System.in);
-    System.out.print("Enter break length in minutes: ");
-    int breakLength = scanner.nextInt();
-    System.out.print("Enter break frequency in minutes: ");
-    int breakFrequency = scanner.nextInt();
 
-    BreakReminderApp app = new BreakReminderApp(breakLength, breakFrequency);
-    app.startSchedule();
 
-    System.out.println("Press 's' to stop the schedule or 'q' to quit.");
-    while (true) {
-        String input = scanner.next();
-        if (input.equals("s")) {
-            app.stopSchedule();
-        } else if (input.equals("q")) {
-            if (app.timer != null) {
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter break length in minutes: ");
+        int breakLength = scanner.nextInt();
+        System.out.print("Enter break frequency in minutes: ");
+        int breakFrequency = scanner.nextInt();
+
+        BreakReminderApp app = new BreakReminderApp(breakLength, breakFrequency);
+        app.startSchedule();
+
+        System.out.println("Press 's' to stop the schedule or 'q' to quit.");
+        while (true) {
+            String input = scanner.next();
+            if (input.equals("s")) {
                 app.stopSchedule();
+            } 
+            else if (input.equals("q")) {
+                if (app.isRunning()) {
+                    app.stopSchedule();
+                }
+                break;
             }
-            break;
         }
+
+        scanner.close();
     }
-
-    scanner.close();
-
 }
